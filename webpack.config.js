0c3ac8cb245673853e1a94ad2e38ffe3b6444ba1
nanero42 <jacobadlerman@gmail.com>
2021-02-27 16:01:43 +0300

    finish project config

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = (env, argv) => {
+
+    const isProd = argv.mode === 'production'
+    const isDev = !isProd
+
+    console.log('isProd', isProd);
+    console.log('isDev', isDev);
+
+    const filename = ext => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+
+    const plugins = () => {
+        const base = [ //в массив const можно добавлять новые элементы через push
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: filename('css')
+            }),
+            new HtmlWebpackPlugin({
+                template: './index.html'
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'src', 'favicon.ico'),
+                        to: path.resolve(__dirname, 'dist')
+                    },
+                ]
+            })
+        ]
+
+        if (isDev) {
+            base.push(new ESLintPlugin())
+        }
+
+        return base
+    }
+
+    return {
+        target: 'web',
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            main: ['@babel/polyfill', '/index.js']
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: filename('js')
+        },
+        resolve: {
+            extensions: ['.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@core': path.resolve(__dirname, 'core')
+            }
+        },
+        devServer: {
+            port: '3000',
+            open: true, //browser will auto open
+            hot: true,
+            // watchContentBase: true
+        },
+        devtool: isDev ? 'source-map' : false,
+        plugins: plugins(),
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/i,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}
